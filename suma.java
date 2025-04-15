e59c73fcdee2c0c280ac549481e130b0ba573409
Mauricio  Buitrago Agudelo <mauriciobuitrago163028@gmail.com>
2025-04-14 20:06:28 -0500

    creacion del archivo suma

---
+++ suma.java
@@ -0,0 +1,18 @@
+
+public class suma {
+
+    public static void main(String[] args) {
+        
+            int suma,n1,n2;
+
+
+            n1=5;
+            n2=10;
+
+            suma = n1+n2;
+
+
+            System.out.println("el resultado de la suma es:" +suma);
+
+    }
+}
