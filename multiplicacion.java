08c140106157711d9e13031e72e6943d39d69b37
Mauricio  Buitrago Agudelo <mauriciobuitrago163028@gmail.com>
2025-04-14 20:30:59 -0500

    creacion archivo multiplicacion

---
+++ multiplicacion.java
@@ -0,0 +1,16 @@
+public class multiplicacion {
+    
+    
+    public static void main(String[] args) {
+        
+        int n3,n4,multiplicacion;
+
+        n3=45;
+        n4=10;
+
+        multiplicacion= n3-n4;
+
+
+        System.out.println("el resultado de la multiplicacion es:"+ multiplicacion);
+    }
+}
